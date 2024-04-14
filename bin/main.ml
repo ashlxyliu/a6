773a5b5e3891bf015366aae49e853e8da7e9ba04
Ashley Liu <awl77@cornell.edu>
2024-04-13 21:01:03 -0400

    Implement

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,3 @@
-(** @author Ashley Liu awl77 *)
-
 open Unix
 open A6.Rbset
 
@@ -9,7 +7,6 @@ let time f x =
   let stop = gettimeofday () in
   stop -. start
 
-(* Function to insert n distinct elements into a set *)
 let insert_n_elements n =
   let rec insert_aux set current_element =
     if current_element < n then
@@ -18,13 +15,11 @@ let insert_n_elements n =
   in
   insert_aux empty 0
 
-(* Return the median time *)
 let measure_insertion n =
   let times = Array.init 10 (fun _ -> time insert_n_elements n) in
   Array.sort compare times;
   times.(Array.length times / 2)
 
-(* Generating a range of n values for testing *)
 let generate_n_values min_n max_n step =
   let rec aux current acc =
     if current > max_n then List.rev acc
@@ -33,15 +28,21 @@ let generate_n_values min_n max_n step =
   aux min_n []
 
 let () =
+  let output_filename =
+    if Array.length Sys.argv > 1 then Sys.argv.(1)
+    else "performance_data.csv" (* Default filename if none specified *)
+  in
   let min_n = 1000 in
-  (* Adjust based on your system's performance *)
   let max_n = 1000000 in
   let step = 10 in
   let n_values = generate_n_values min_n max_n step in
-  Printf.printf "N,Time,N log N\n";
+  let out_channel = open_out output_filename in
+  Printf.fprintf out_channel "N,Time,N log N\n";
   List.iter
     (fun n ->
       let median_time = measure_insertion n in
       let n_log_n = float_of_int n *. log (float_of_int n) in
-      Printf.printf "%d,%g,%g\n" n median_time n_log_n)
-    n_values
+      Printf.fprintf out_channel "%d,%g,%g\n" n median_time n_log_n)
+    n_values;
+  close_out out_channel;
+  Printf.printf "Output written to %s\n" output_filename
